6e62f63a271a70f05faf6f9581e4dbb5706c8cbd
Wilfried Deswarte <W.deswarte@outlook.fr>
2023-03-28 18:35:51 +0200

    Etape 2

--- Calculatrice/Calculatrice.js
+++ Calculatrice/Calculatrice.js
@@ -3,14 +3,16 @@
 //alert('js runnig');
 console.log("Alexis is here!");
 
+/*
 //---------variables---------//
 
 var screen = document.getElementById("screen");
 var numChar; //nombre total de caractères inscrits
 var currentChar, previousChar; // enregistre les actuels et anciens caractères inscrits
-var operations = ['+','-','*','/','%']
+var operations = ['+', '-', '*', '/', '%'];
 
-//---------fonctions---------//
+
+//---------fonctions---------// 
 
 function clearScreen() {
     screen.value = "";
@@ -53,4 +55,77 @@ function overwrite() {
 
 function removeChar(){
     screen.value = screen.value.substring(0, numChar - 1);
+
+   
+} */
+
+
+//---------classe---------//
+class Calculator {
+    constructor() {
+        this.screen = document.getElementById("screen");
+        this.numChar = 0;
+        this.currentChar = '';
+        this.previousChar = '';
+        this.operations = ['+', '-', '*', '/', '%'];
+        this.previousCalculation = [''];
+       
+    }
+
+    clearScreen() {
+        this.screen.value = '';
+        this.numChar = 0;
+        this.currentChar = '';
+        this.previousChar = '';
+    }
+
+    display(c) {
+        this.screen.value += c;
+        this.numChar = this.screen.value.length;
+        this.currentChar = c;
+        this.getPreviousChar();
+    }
+
+    calculate() {
+        this.previousCalculation[0] = this.screen.value;
+        this.screen.value = eval(this.screen.value);
+        console.log('previousCalculation:' + this.previousCalculation[0]);      
+    }
+
+    getPreviousChar() {
+        this.previousChar = this.screen.value.substring(this.numChar - 2, this.numChar - 1);
+        this.checkSyntax();
+    }
+
+    checkSyntax() {
+        if (this.operations.includes(this.currentChar) && this.numChar == 1) {
+            this.removeChar();
+        }
+
+        if (this.operations.includes(this.previousChar) && this.operations.includes(this.currentChar)) {
+            if (this.previousChar == this.currentChar) {
+                alert('Erreur syntaxe');
+                this.removeChar();
+            } else {
+                this.overwrite();
+            }
+        }
+    }
+
+    overwrite() {
+        this.screen.value = this.screen.value.slice(0, this.numChar - 2) + this.screen.value.slice(this.numChar - 1);
+    }
+
+    removeChar() {
+        this.screen.value = this.screen.value.substring(0, this.numChar - 1);
+        this.numChar--;
+        this.currentChar = '';
+        this.previousChar = '';
+
+        if (this.numChar == 0) {
+            this.screen.value = this.previousCalculation;
+        }
+    }
 }
+
+const calculator = new Calculator();
